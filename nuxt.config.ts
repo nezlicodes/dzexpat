05c92d676035e16e4a46876b3173a29bf2b275d3
nezlicodes <benmeradi.lilyanezli@gmail.com>
2026-01-20 11:02:40 +0100

    fox: build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
   i18n: {
     strategy: 'prefix_except_default',
     defaultLocale: 'fr',
-    langDir: 'i18n/locales',
+    langDir: 'locales',
     lazy: true,
     compilation: {
       strictMessage: false
